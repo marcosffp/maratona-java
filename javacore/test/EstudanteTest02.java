0b3f0bd776abdeb26525ef33df3fe8ddcc9a3da3
Marcos Alberto <159904717+marcosffp@users.noreply.github.com>
2024-08-03 19:06:53 -0300

    POO

---
+++ javacore/test/EstudanteTest02.java
@@ -0,0 +1,15 @@
+package javacore.test;
+
+import javacore.domain.Estudante;
+
+public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante estudante=new Estudante();
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        
+    }
+    
+}
